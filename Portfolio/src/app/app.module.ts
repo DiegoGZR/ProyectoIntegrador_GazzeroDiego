e6cdff7953c5d34d97ea2f512cbf63ef201d8420
DiegoGZR <109150211+DiegoGZR@users.noreply.github.com>
2023-01-19 16:32:11 -0300

    HeaderLogoSocialBanner

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoAPComponent,
+    HeaderComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
