1f60f81a5094c14720c7794cac8724fbc492f031
DiegoGZR <109150211+DiegoGZR@users.noreply.github.com>
2023-01-23 10:10:58 -0300

    230123

---
+++ Portfolio/BackEnd/src/test/java/com/portfolio/dgg/DggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
