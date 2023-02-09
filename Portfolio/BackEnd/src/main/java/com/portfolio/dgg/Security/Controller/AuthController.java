7fb83f5656cedc4f0a53d20d58a59d214eb679b5
DiegoGZR <109150211+DiegoGZR@users.noreply.github.com>
2023-02-09 10:39:14 -0300

    AuthController

---
+++ Portfolio/BackEnd/src/main/java/com/portfolio/dgg/Security/Controller/AuthController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.dgg.Security.Controller;
+
+/**
+ *
+ * @author dmisi
+ */
+public class AuthController {
+    
+}
